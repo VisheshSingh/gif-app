e60dc1f4ec29bf92df9771a52e3d7b79e0e18716
Vishesh Singh <VisheshSingh16@gmail.com>
2020-09-27 11:53:11 -0500

    custom hook - useGif

---
+++ src/hooks/useGif.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+const useGif = (tag = 'cats') => {
+  const [gif, setGif] = useState();
+  const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=${tag}`;
+
+  const fetchGif = async () => {
+    const response = await fetch(url);
+    const resData = await response.json();
+    const imageSrc = resData.data.images.downsized_large.url;
+    setGif(imageSrc);
+  };
+
+  useEffect(() => {
+    fetchGif();
+    // eslint-disable-next-line
+  }, []);
+
+  const handleClick = () => {
+    fetchGif();
+  };
+
+  return { gif, handleClick };
+};
+
+export default useGif;
