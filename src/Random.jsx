9414872017a271df7f27ed881b908eea8db361d8
Vishesh Singh <VisheshSingh16@gmail.com>
2020-09-27 11:56:29 -0500

    useGif in Random component

--- src/Random.jsx
+++ src/Random.jsx
@@ -1,24 +1,8 @@
-import React, { useState, useEffect } from 'react';
-
-const API_KEY = process.env.REACT_APP_API_KEY;
+import React from 'react';
+import useGif from './hooks/useGif';
 
 const Random = () => {
-  const [gif, setGif] = useState();
-  const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
-  const fetchGif = async () => {
-    const response = await fetch(url);
-    const resData = await response.json();
-    const imageSrc = resData.data.images.downsized_large.url;
-    setGif(imageSrc);
-  };
-  useEffect(() => {
-    fetchGif();
-    // eslint-disable-next-line
-  }, []);
-
-  const handleClick = () => {
-    fetchGif();
-  };
+  const { gif, handleClick } = useGif();
 
   return (
     <div className='random-gif'>
