3c74a975c19e96d7883d62cda72c11a426b089dd
Vishesh Singh <VisheshSingh16@gmail.com>
2020-09-27 11:58:03 -0500

    useGif in Tag component

--- src/Tag.jsx
+++ src/Tag.jsx
@@ -1,26 +1,9 @@
-import React, { useState, useEffect } from 'react';
-
-const API_KEY = process.env.REACT_APP_API_KEY;
+import React, { useState } from 'react';
+import useGif from './hooks/useGif';
 
 const Tag = () => {
-  const [gif, setGif] = useState();
   const [tag, setTag] = useState('dogs');
-  const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=${tag}`;
-
-  const fetchGif = async () => {
-    const response = await fetch(url);
-    const resData = await response.json();
-    const imageSrc = resData.data.images.downsized_large.url;
-    setGif(imageSrc);
-  };
-  useEffect(() => {
-    fetchGif();
-    // eslint-disable-next-line
-  }, []);
-
-  const handleClick = () => {
-    fetchGif();
-  };
+  const { gif, handleClick } = useGif(tag);
 
   return (
     <div className='tag-gif'>
